563a38b0140c4d246e59ad07f9aa1c9a16651ac1
hmp-anthony <anthonygoddard962@gmail.com>
2023-01-17 17:38:12 +0000

    changed order of imprts

--- examples/import_test.cpp
+++ examples/import_test.cpp
@@ -1,19 +1,17 @@
 #include <Eigen/Dense>
+#include <autodiff/gradient.hpp>
 #include <iostream>
 
-#include "autodiff/var.hpp"
-
-using Eigen::MatrixXd;
 using autodiff::base::var;
+using Eigen::MatrixXd;
 
 int main() {
-
-    // test that autodiff has been imported 
+    // test that autodiff has been imported
     var a(10);
     var b(20);
     auto c = a * b;
 
-    // test that Eigen has been imported 
+    // test that Eigen has been imported
     MatrixXd m(2, 2);
     m(0, 0) = 3;
     m(1, 0) = 2.5;
